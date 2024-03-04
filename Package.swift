b32a3acd93bec11068989fca1eeae5066dd383fa
John Holdsworth <>
2024-03-04 04:11:41 +0100

    Update 1.0.9003

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 import CompilerPluginSupport
 import PackageDescription
 
-let repo = "https://github.com/Concoction/binary-SwiftUIX/raw/1.0.9002/"
-let modules = []
+let repo = "https://github.com/Concoction/binary-SwiftUIX/raw/1.0.9003/"
+let modules = ["SwiftUIX", "_SwiftUIX"]
 
 let package = Package(
     name: "SwiftUIX",
@@ -23,5 +23,15 @@ let package = Package(
 
     targets: [
         .target(name: "SwiftUIX_Binder", dependencies: modules.map { .target(name: $0) }),
+        .binaryTarget(
+            name: "SwiftUIX",
+            url: repo + "SwiftUIX.xcframework.zip",
+            checksum: "7ef0e9ca0653fcf3792a5c2580332120abbc9876c62aff251f3691d16a4d8924"
+        ),
+        .binaryTarget(
+            name: "_SwiftUIX",
+            url: repo + "_SwiftUIX.xcframework.zip",
+            checksum: "03bdcb39832b7840458ff7356429421799157f607b0825b3b8959c6ca9f72c61"
+        ),
     ]
 )
